6a7bc3e8623d262de76aa2c4b41872335a8825cb
Claire <claire.github-309c@sitedethib.com>
2021-09-01 22:06:40 +0200

    Fix processing mentions to domains with non-ascii TLDs (#16689)
    Fixes #16602

--- app/models/account.rb
+++ app/models/account.rb
@@ -63,7 +63,6 @@ class Account < ApplicationRecord
 
   USERNAME_RE   = /[a-z0-9_]+([a-z0-9_\.-]+[a-z0-9_]+)?/i
   MENTION_RE    = /(?<=^|[^\/[:word:]])@((#{USERNAME_RE})(?:@[[:word:]\.\-]+[[:word:]]+)?)/i
-  URL_PREFIX_RE = /\Ahttp(s?):\/\/[^\/]+/
 
   include Attachmentable
   include AccountAssociations
