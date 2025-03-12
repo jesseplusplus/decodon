cc7c221df08c3a5b204f5c0cc7124981b3d6c8a4
Jesse Karmani <jessekarmani@gmail.com>
2025-03-12 13:13:38 -0700

    Use filtered items to check for next page in ActivityPub context

--- app/controllers/activitypub/contexts_controller.rb
+++ app/controllers/activitypub/contexts_controller.rb
@@ -36,7 +36,7 @@ class ActivityPub::ContextsController < ActivityPub::BaseController
       type: :unordered,
       part_of: context_items_url(@conversation),
       next: next_page,
-      items: @conversation.statuses.map { |status| status.local? ? status : status.uri }
+      items: @items.map { |status| status.local? ? status : status.uri }
     )
 
     return page if page_requested?
@@ -53,9 +53,9 @@ class ActivityPub::ContextsController < ActivityPub::BaseController
   end
 
   def next_page
-    return nil if @conversation.statuses.size < DESCENDANTS_LIMIT
+    return nil if @items.size < DESCENDANTS_LIMIT
 
-    context_items_url(@conversation, page: true, min_id: @conversation.statuses.last.id)
+    context_items_url(@conversation, page: true, min_id: @items.last.id)
   end
 
   def page_params
